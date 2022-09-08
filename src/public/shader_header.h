8e210f9746629a2b2a392fd2ad64276997baebab
Aria Golliver <aria.golliver@gmail.com>
2022-09-08 15:55:12 -0700

    add a template to be auto-added to new files

--- src/public/shader_header.h
+++ src/public/shader_header.h
@@ -36,5 +36,17 @@ vec2 _norm_mouse() {
 )";
     std::string headerEnd = "//#####$$$$$ END HEADER $$$$$#####//\n";
 
+    std::string shaderTemplate = R"(
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = .5 + .5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
+    
+    fragColor = vec4(col, 1.0);
+}
+)";
 }
 
